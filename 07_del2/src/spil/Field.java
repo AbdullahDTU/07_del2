dec04be7c2c369de9a1fb797472b91dd045f2a4d
Abdullah <s203234@dtu.dk>
2021-10-09 14:13:34 +0200

    Added Field Class, changed player variables and values

---
+++ 07_del2/src/spil/Field.java
@@ -0,0 +1,14 @@
+package spil;
+
+public class Field {
+
+    public static void main(String[] args) {
+        //The gold amount of the different Fields
+        int Tower = 250;
+
+        //Flavor text of each field
+        String TowerText = "You have entered the Tower and found a chest with " + Tower + " Gold pieces";
+
+        System.out.println(TowerText);
+    }
+}
