f665393b7211046add14995d558de104e9b5744e
Jessica <s185192@dtu.dk>
2021-10-11 19:47:58 +0200

    add Cold Desert and Walled City!!!

--- 07_del2/src/spil/Field.java
+++ 07_del2/src/spil/Field.java
@@ -7,16 +7,23 @@ public class Field {
         int Tower = 250;
         int Crater = 100;
         int PalaceGates = 100;
+        int ColdDesert=20;
+        int WalledCity=180;
+
 
         //Flavor text of each field
         String TowerText = "You have entered the Tower and found a chest with " + Tower + " Gold pieces";
         String CraterText = "You have fallen down a Crater! " + Crater + " of your gold pieces have fallen out of your pocket on the way down";
-        String PalaceGates = "You have entered through the Palace Gates. It's crowded and you manage to steal a gold pouch containing " + PalaceGates + " gold pieces from a man passing by";
-
+        String PalaceGatesText = "You have entered through the Palace Gates. It's crowded and you manage to steal a gold pouch containing " + PalaceGates + " gold pieces from a man passing by";
+        String ColdDesertText= "You have entered the Cold Desert and we will take " + ColdDesert + " Gold pieces" ;
+        String WalledCityText= "You have entered the Walled City and for that you get " + WalledCity + " Gold pieces";
         //Printing out flavor text
         System.out.println(TowerText);
         System.out.println(CraterText);
         System.out.println(PalaceGatesText);
+        System.out.println(ColdDesertText);
+        System.out.println(WalledCityText);
+      
 
 
 
