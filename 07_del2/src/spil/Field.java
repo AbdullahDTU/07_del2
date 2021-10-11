034aae035a725cf6a11d9dc6b6f97daa32d23276
Baime <s215790@dtu.dk>
2021-10-11 20:17:00 +0200

    add field

--- 07_del2/src/spil/Field.java
+++ 07_del2/src/spil/Field.java
@@ -7,8 +7,15 @@ public class Field {
         int Tower = 250;
         int Crater = 100;
         int PalaceGates = 100;
+<<<<<<< Updated upstream
         int ColdDesert = 20;
         int WalledCity = 180;
+=======
+        int ColdDesert= 20;
+        int WalledCity= 180;
+        int Monastary = 0;
+        int BlackCave =-70;
+>>>>>>> Stashed changes
 
 
         //Flavor text of each field
@@ -17,6 +24,9 @@ public class Field {
         String PalaceGatesText = "You have entered through the Palace Gates. It's crowded and you manage to steal a gold pouch containing " + PalaceGates + " gold pieces from a man passing by";
         String ColdDesertText = "You have entered the Cold Desert and we will take " + ColdDesert + " Gold pieces";
         String WalledCityText = "You have entered the Walled City and for that you get " + WalledCity + " Gold pieces";
+        String MonastaryText = "You have now reached a Monastery, you received" + Monastary + "coins";
+        String BlackCaveText = "Watch out! BlackCave ahead, you will now be punish by" + BlackCave + "coins";
+
 
         //Printing out flavor text
         System.out.println(TowerText);
@@ -24,6 +34,8 @@ public class Field {
         System.out.println(PalaceGatesText);
         System.out.println(ColdDesertText);
         System.out.println(WalledCityText);
+        System.out.println(MonastaryText);
+        System.out.println(BlackCaveText);
 
 
     }
