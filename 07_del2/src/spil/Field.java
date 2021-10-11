b05f96a5eb64556e2d014aa6d6e8492e13d430b9
haugaard1101 <s205434@student.dtu.dk>
2021-10-11 19:21:56 +0200

    Added Crater and PalaceGates

--- 07_del2/src/spil/Field.java
+++ 07_del2/src/spil/Field.java
@@ -3,12 +3,35 @@ package spil;
 public class Field {
 
     public static void main(String[] args) {
-        //The gold amount of the different Fields
+        //The gold amount of the different fields
         int Tower = 250;
+        int Crater = 100;
+        int PalaceGates = 100;
 
         //Flavor text of each field
         String TowerText = "You have entered the Tower and found a chest with " + Tower + " Gold pieces";
+        String CraterText = "You have fallen down a Crater! " + Crater + " of your gold pieces have fallen out of your pocket on the way down";
+        String PalaceGates = "You have entered through the Palace Gates. It's crowded and you manage to steal a gold pouch containing " + PalaceGates + " gold pieces from a man passing by";
 
+        //Printing out flavor text
         System.out.println(TowerText);
+        System.out.println(CraterText);
+        System.out.println(PalaceGatesText);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
