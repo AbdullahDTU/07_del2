f93faf3446fbdb57bc972bf6598f4bdab0313738
Abdu <Abdu0901@ltkskole.dk>
2021-10-12 13:35:31 +0200

    Added The Pit and Goldmine Fields

--- 07_del2/src/spil/Field.java
+++ 07_del2/src/spil/Field.java
@@ -13,7 +13,9 @@ public class Field {
         int BlackCave = 70;
         int HutsInTheMountain = 60;
         int TheWerewall = 80;
-        
+        int ThePit = 50;
+        int GoldMine = 650;
+
         //Flavor text of each field
         String TowerText = "You have entered the Tower and found a chest with " + Tower + " Gold pieces";
         String CraterText = "You have fallen down a Crater! " + Crater + " of your gold pieces have fallen out of your pocket on the way down";
@@ -24,8 +26,8 @@ public class Field {
         String BlackCaveText = "Watch out! BlackCave ahead, you will now be punish by" + BlackCave + "coins";
         String HutsInTheMountainText = "You found some Huts in the Moutain with " + HutsInTheMountain + "leftover coins";
         String TheWerewallText = "You have come across the werewall. You lose" + TheWerewall + "coins but you get an extra turn";
-
-
+        String ThePitText = "You barely hang into the edge to prevent yourself falling into the Pit " + ThePit + " Gold pieces slip out of your pocket";
+        String GoldMineText = "You stumple upon a mine filled with Gold you " + GoldMine + " Gold pieces";
 
         //Printing out flavor text
         System.out.println(TowerText);
@@ -37,7 +39,8 @@ public class Field {
         System.out.println(BlackCaveText);
         System.out.println(HutsInTheMountainText);
         System.out.println(TheWerewallText);
-
+        System.out.println(ThePitText);
+        System.out.println(GoldMineText);
 
 
     }
