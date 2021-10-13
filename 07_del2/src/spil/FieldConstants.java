62eacb2420e6e09084860cbcb8672883b29b4143
Abdu <Abdu0901@ltkskole.dk>
2021-10-13 11:01:05 +0200

    Changed Gold to Coins in FlavorText

--- 07_del2/src/spil/FieldConstants.java
+++ 07_del2/src/spil/FieldConstants.java
@@ -30,15 +30,15 @@ public class FieldConstants {
     public static final String GoldMineName = "Gold Mine";
 
     //Flavor text of each field
-    public static final String TowerText = "You have entered the Tower and found a chest with " + Tower + " Gold pieces";
-    public static final String CraterText = "You have fallen down a Crater! " + Crater + " of your gold pieces have fallen out of your pocket on the way down";
-    public static final String PalaceGatesText = "You have entered through the Palace Gates. It's crowded and you manage to steal a gold pouch containing " + PalaceGates + " gold pieces from a man passing by";
-    public static final String ColdDesertText = "You have entered the Cold Desert and we will take " + ColdDesert + " Gold pieces";
-    public static final String WalledCityText = "You have entered the Walled City and for that you get " + WalledCity + " Gold pieces";
+    public static final String TowerText = "You have entered the Tower and found a chest with " + Tower + " Coins";
+    public static final String CraterText = "You have fallen down a Crater! " + Crater + " of your Coins have fallen out of your pocket on the way down";
+    public static final String PalaceGatesText = "You have entered through the Palace Gates. It's crowded and you manage to steal a pouch containing " + PalaceGates + " Coins from a man passing by";
+    public static final String ColdDesertText = "You have entered the Cold Desert and we will take " + ColdDesert + " Coins";
+    public static final String WalledCityText = "You have entered the Walled City and for that you get " + WalledCity + " Coins";
     public static final String MonastaryText = "You have now reached a Monastery, you received" + Monastary + "coins";
     public static final String BlackCaveText = "Watch out! BlackCave ahead, you will now be punish by" + BlackCave + "coins";
     public static final String HutsInTheMountainText = "You found some Huts in the Moutain with " + HutsInTheMountain + "leftover coins";
     public static final String TheWerewallText = "You have come across the werewall. You lose" + TheWerewall + "coins but you get an extra turn";
-    public static final String ThePitText = "You barely hang into the edge to prevent yourself falling into the Pit " + ThePit + " Gold pieces slip out of your pocket";
-    public static final String GoldMineText = "You stumple upon a mine filled with Gold you " + GoldMine + " Gold pieces";
+    public static final String ThePitText = "You suddenly feel your legs dissapear under you. You barely hang into the ledge to prevent yourself falling into the Pit, though " + ThePit + " Coins slip out of your pocket";
+    public static final String GoldMineText = "You stumple upon a mine filled with Gold you sell for " + GoldMine + " Coins";
 }
